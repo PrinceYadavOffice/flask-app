61fe618aa5ecf66fc6f0c1c80314148f6d445760
prince <prince.yadav@tothenew.com>
2023-02-21 16:32:38 +0530

    added migrations

--- resources/user.py
+++ resources/user.py
@@ -1,12 +1,13 @@
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required, create_refresh_token, get_jwt_identity
 from flask import request
 from db import db
 from models import UserModel
 from schemas import UserSchema
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from blocklist import BLOCKLIST                                                                                                                                                                                         
 
 blp = Blueprint("Users", "users", description="Operations on Users")
 
@@ -19,7 +20,7 @@ class UserRegister(MethodView):
             abort(409, message="A user with that username already exists.")
 
         user = UserModel(
-            username=user_data["username"],
+            username=user_data["username"],                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
         db.session.add(user)
@@ -51,7 +52,29 @@ class UserLogin(MethodView):
         ).first()
 
         if user and pbkdf2_sha256.verify(user_data["password"], user.password):
-            access_token = create_access_token(identity=user.id)
-            return {"access_token": access_token}, 200
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
 
         abort(401, message="Invalid credentials.")
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        # Make it clear that when to add the refresh token to the blocklist will depend on the app design
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"access_token": new_token}, 200
